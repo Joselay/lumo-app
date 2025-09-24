1ab4f7993275eb8a66e3517103869c68f5516300
MENGLAY <tongmenglaysmae5@gmail.com>
2025-09-24 22:04:32 +0700

    Integrate HomePage into ShadApp in main.dart to set the initial home screen for the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'features/home/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const LumoApp());
@@ -10,6 +11,8 @@ class LumoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShadApp();
+    return ShadApp(
+      home: const HomePage(),
+    );
   }
 }
